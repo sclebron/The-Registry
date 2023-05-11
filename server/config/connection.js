6b3d2274a9a7f3edf8695ab2d95088362e5f4dd2
SLeBron <sophieclebron@gmail.com>
2023-05-11 11:29:28 -0600

    added railway connection

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,11 +1,19 @@
 const mongoose = require("mongoose");
 
 mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/registryDB",
+  process.env.MONGODB_URI || "mongo:AHRb6E98BSKBypwA6sL3@containers-us-west-75.railway.app:6652",
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   }
 );
+
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/registryDB",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }
+// );
 
 module.exports = mongoose.connection;
